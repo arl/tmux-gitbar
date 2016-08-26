e892c73075e22f9bf57df177c08feccb4b7ebeda
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-08-27 00:34:20 +0200

    shellcheck [SC2086]: double quote

--- test/integration-tests/run_integration_tests.sh
+++ test/integration-tests/run_integration_tests.sh
@@ -4,7 +4,7 @@ export TMUXCONF="$PWD/tmux.conf"
 
 # Create a minimal tmux conf file
 > "${TMUXCONF}"
-echo TMUX_GITBAR_DIR=\"$PWD\" >> "${TMUXCONF}"
-echo source-file \"$PWD/tmux-gitbar.tmux\" >> "${TMUXCONF}"
+echo TMUX_GITBAR_DIR=\""$PWD"\" >> "${TMUXCONF}"
+echo source-file \""$PWD/tmux-gitbar.tmux"\" >> "${TMUXCONF}"
 
 bats test/integration-tests/main.bats
