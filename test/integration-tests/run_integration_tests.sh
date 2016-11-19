c98c06fee67de3ad6e4ad8ffe3f46152de3f04f5
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-11-19 23:43:59 +0100

    Fix integration test: make tmgb.conf path absolute

--- test/integration-tests/run_integration_tests.sh
+++ test/integration-tests/run_integration_tests.sh
@@ -5,6 +5,7 @@ export TMUXCONF="$PWD/tmux.conf"
 # Create a minimal tmux conf file
 > "${TMUXCONF}"
 echo TMUX_GITBAR_DIR=\""$PWD"\" >> "${TMUXCONF}"
+echo TMUX_GITBAR_CONF=\""$PWD/tmux-gitbar.conf"\" >> "${TMUXCONF}"
 echo source-file \""$PWD/tmux-gitbar.tmux"\" >> "${TMUXCONF}"
 
 bats test/integration-tests/main.bats
